da3e90a19fa4b815b9158d32f788ab6c42767b6d
RISHI <rishilama03@gmail.com>
2022-08-26 17:08:35 +0530

    product-page-done

---
+++ src/components/ProductContainer/ProductContainer.js
@@ -0,0 +1,227 @@
+import React from 'react'
+
+function ProductContainer() {
+    return (
+        <div className='product-container'>
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+            <a href='/' className='single-product__card'>
+                <div className='single-product__image-container'>
+                    <img src='/images/IMG_6269-420x420.webp' alt='img' className='product-image' />
+                </div>
+                
+                <div className='single-product__details-container'>
+                    <p className='brand'>Asics</p>
+                    <p className='name'>ICE STUDIO GEL-KAYANO 14</p>
+                    <div className='more-details'>
+                        <p className='price'>$150.00</p>
+                        <p className='stock'>SOLD OUT</p>
+                    </div>
+                </div>
+
+                <div className='size-details'>
+                    <p>41 &nbsp; 42 &nbsp; 43 &nbsp; 44 &nbsp; 45</p>
+                </div>
+            </a>
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default ProductContainer
--- src/pages/SneakerZone/SneakerZone.js
+++ src/pages/SneakerZone/SneakerZone.js
@@ -2,12 +2,14 @@ import React from 'react'
 import SideBar from '../../components/SideBar/SideBar'
 import './SneakerZone.css'
 import SneakerPageSideBarData from '../../dummy data/SneakerPageSideBarData'
+import ProductContainer from '../../components/ProductContainer/ProductContainer'
 
 function SneakerZone() {
     return (
         <div className='container'>
             <div className='page__content'>
                 <SideBar data={SneakerPageSideBarData} />
+                <ProductContainer />
             </div>
         </div>
     )
