26e8a77840d9a93f241c52548d440ff147fc36b2
RISHI <rishilama03@gmail.com>
2022-08-28 15:36:03 +0530

    .

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <Link to='/'>HomePage</Link>
+      <Link to='/sneakerzone'>Sneaker Zone</Link>
+      <Link to='/watchex'>Watchex</Link>
+    </>
   )
 }
 
