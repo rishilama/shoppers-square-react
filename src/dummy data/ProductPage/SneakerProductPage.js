2627ec92de8a0f526f9c2b2621ebfe5a30b6c29a
RISHI <rishilama03@gmail.com>
2022-09-05 09:47:41 +0530

    .

--- src/dummy data/ProductPage/SneakerProductPage.js
+++ src/dummy data/ProductPage/SneakerProductPage.js
@@ -3,7 +3,10 @@ const SneakerProductPage = [
         id: 0,
         name: "Old Skool ' Tyga Wavy Baby '",
         brand: "Vans",
-        image : "images/bulk-image-crop/vtwb.jpg",
+        image : [
+            "images/bulk-image-crop/vtwb.jpg",
+            "images/bulk-image-crop/vtwb2.jpg"
+        ],
         price: "₹3299",
         sizes: "40-41-42-43-44-45",
         category : ["Vans", "Men's"],
@@ -21,6 +24,15 @@ const SneakerProductPage = [
     
     {
         id: 2,
+        name: "Air Force 1 'Triple Black'",
+        brand: "Nike",
+        image : "images/bulk-image-crop/vtwb.jpg",
+        price: "₹1700",
+        category : ["Nike", "Women's"],
+        inStock : 0
+    },
+    {
+        id: 3,
         name: "Air Force 1 'Triple Black'",
         brand: "Nike",
         image : "images/bulk-image-crop/vtwb.jpg",
